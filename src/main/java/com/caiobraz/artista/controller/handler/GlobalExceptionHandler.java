da3ae9267b957432f81e954fb50fff21ff45e4fe
Caio Braz <caiooliveirabraz@hotmail.com>
2026-01-29 15:35:17 -0400

    Melhorado tratamento de mensagens globais
    - Incluido mensagens por chave para exceções  de 'SystemException'
    - Adicionado messages.properties

--- src/main/java/com/caiobraz/artista/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/com/caiobraz/artista/controller/handler/GlobalExceptionHandler.java
@@ -5,12 +5,12 @@ import java.util.Locale;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceResolvable;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -26,6 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 import com.caiobraz.artista.service.exception.AuthException;
 import com.caiobraz.artista.service.exception.BusinessException;
 import com.caiobraz.artista.service.exception.NotFoundException;
+import com.caiobraz.artista.service.exception.SystemException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -34,39 +35,13 @@ public class GlobalExceptionHandler {
 
     private final MessageSource messageSource;
 
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleSecurityException(Exception exception) {
-        ProblemDetail errorDetail = null;
-        log.warn(exception.getMessage());
+        log.error(exception.getMessage(), exception);
 
-        if (exception instanceof BadCredentialsException) {
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, exception.getMessage());
-            errorDetail.setProperty("mensagem", "Usuário ou senha incorreto");
-
-            return errorDetail;
-        }
-
-        if (exception instanceof AccountStatusException) {
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, exception.getMessage());
-            errorDetail.setProperty("mensagem", "Conta bloqueada");
-        }
-
-        if (exception instanceof AccessDeniedException) {
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, exception.getMessage());
-            errorDetail.setProperty("mensagem", "Sem autorização para acessar esse recurso");
-        }
-
-        if (exception instanceof SignatureException) {
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, exception.getMessage());
-            errorDetail.setProperty("mensagem", "Token JWT inválido");
-        }
-
-        if (errorDetail == null) {
-            log.error(exception.getMessage(), exception);
-
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
-            errorDetail.setProperty("mensagem", "Erro interno");
-        }
+        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        errorDetail.setProperty("mensagem", getMessage("erro.interno"));
 
         return errorDetail;
     }
@@ -77,7 +52,51 @@ public class GlobalExceptionHandler {
         log.error(exception.getMessage(), exception);
 
         ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, exception.getMessage());
-        errorDetail.setProperty("mensagem", "Token JWT expirado");
+        errorDetail.setProperty("mensagem", getMessage("auth.tokenExpirado"));
+
+        return errorDetail;
+    }
+
+    @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(BadCredentialsException.class)
+    public ProblemDetail handleBadCredentialsException(BadCredentialsException exception) {
+        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, exception.getMessage());
+        errorDetail.setProperty("mensagem", getMessage("auth.usuarioIncorreto"));
+
+        return errorDetail;
+    }
+
+    @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(SignatureException.class)
+    public ProblemDetail handleSignatureException(SignatureException exception) {
+        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, exception.getMessage());
+        errorDetail.setProperty("mensagem", getMessage("auth.tokenInvalido"));
+
+        return errorDetail;
+    }
+
+    @ResponseStatus(code = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AuthException.class)
+    public ProblemDetail handleAccessDeniedException(AuthException exception) {
+        String mensagem = getMessage(exception);
+
+        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, mensagem);
+    }
+
+    @ResponseStatus(code = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AccountStatusException.class)
+    public ProblemDetail handleSignatureException(AccountStatusException exception) {
+        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, exception.getMessage());
+        errorDetail.setProperty("mensagem", getMessage("auth.contaBloqueada"));
+
+        return errorDetail;
+    }
+
+    @ResponseStatus(code = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public ProblemDetail handleAccessDeniedException(AccessDeniedException exception) {
+        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, exception.getMessage());
+        errorDetail.setProperty("mensagem", getMessage("auth.semAutorizacao"));
 
         return errorDetail;
     }
@@ -85,29 +104,21 @@ public class GlobalExceptionHandler {
     @ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(BusinessException.class)
     public ProblemDetail handleBusinessException(BusinessException exception) {
-        String mensagem = messageSource.getMessage(exception.getMessage(), exception.getArgs(), this.getLocale());
+        String mensagem = getMessage(exception);
         return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, mensagem);
     }
 
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public ProblemDetail handleNotFoundException(NotFoundException exception) {
-        String mensagem = messageSource.getMessage(exception.getMessage(), exception.getArgs(), this.getLocale());
+        String mensagem = getMessage(exception);
         return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, mensagem);
-    }
-
-    @ResponseStatus(code = HttpStatus.FORBIDDEN)
-    @ExceptionHandler(AuthException.class)
-    public ProblemDetail handleAccessDeniedException(AuthException exception) {
-        String mensagem = messageSource.getMessage(exception.getMessage(), exception.getArgs(), this.getLocale());
-
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, mensagem);
     }
 
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HandlerMethodValidationException.class)
     public ProblemDetail handleNotFoundException(HandlerMethodValidationException exception) {
-        String mensagem = messageSource.getMessage("erro.dadosInvalidos", null, this.getLocale());
+        String mensagem = getMessage("erro.dadosInvalidos");
         ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, mensagem);
         for (ParameterValidationResult result : exception.getValueResults()) {
             for (MessageSourceResolvable resolvableError : result.getResolvableErrors()) {
@@ -137,7 +148,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(org.springframework.dao.DataIntegrityViolationException.class)
     public ProblemDetail handleNotFoundException(org.springframework.dao.DataIntegrityViolationException exception) {
         log.error(exception.getMessage(), exception);
-        String mensagem = messageSource.getMessage("erro.interno", null, Locale.getDefault());
+        String mensagem = getMessage("erro.interno");
 
         ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, mensagem);
         errorDetail.setProperty("mensagem", mensagem);
@@ -145,8 +156,23 @@ public class GlobalExceptionHandler {
         return errorDetail;
     }
 
+    private String getMessage(Exception exception) {
+        if (exception instanceof SystemException systemException) {
+            try {
+                return messageSource.getMessage(systemException.getMessage(), systemException.getArgs(), this.getLocale());
+            } catch (NoSuchMessageException e) {
+                return exception.getMessage();
+            }
+        }
+
+        return exception.getMessage();
+    }
+
+    private String getMessage(String key) {
+        return messageSource.getMessage(key, null, this.getLocale());
+    }
+
     private Locale getLocale() {
         return Locale.getDefault();
     }
-
 }
